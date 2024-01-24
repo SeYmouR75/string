a5b5d69236f5b332342977dbc63f40beea9c53ac
Cainvene <cainvene@student.21-school.ru>
2024-01-24 20:48:23 +0600

    fix

--- src/s21_insert.c
+++ src/s21_insert.c
@@ -1,20 +1,23 @@
 #include "s21_string.h"
 
-void *s21_insert(const char *src, const char *str, s21_size_t start_index) {
-  char *result = S21_NULL;
-  if (str != S21_NULL && src != S21_NULL) {
-    s21_size_t size = s21_strlen(src) + s21_strlen(str);
-    s21_size_t end_index = start_index + s21_strlen(str);
-    result = (char *)calloc(size + 1, sizeof(char));
-    if (result != S21_NULL && start_index < s21_strlen(src)) {
-      for (s21_size_t i = 0; i < start_index; i++) result[i] = src[i];
-      for (s21_size_t i = start_index, j = 0; i < end_index; i++, j++)
-        result[i] = str[j];
-      for (s21_size_t i = end_index, j = start_index; i < size; i++, j++)
-        result[i] = src[j];
-    } else
-      result = S21_NULL;
+void *s21_insert(const char *src, const char *str, size_t start_index) {  if (src == S21_NULL || str == S21_NULL || start_index > s21_strlen(src)) {
+    return S21_NULL;
   }
-  return result;
-  
+  char *ptr = S21_NULL;
+  if (src != S21_NULL && str != S21_NULL) {
+    ptr =
+        (char *)malloc((s21_strlen(src) + s21_strlen(str) + 1) * sizeof(char));
+    if (ptr != S21_NULL && start_index <= s21_strlen(src)) {
+      s21_memcpy(ptr, src, start_index);  
+                                          
+      s21_memcpy(ptr + start_index, str,
+                 s21_strlen(str));  /
+      s21_memcpy(ptr + start_index + s21_strlen(str),
+                 src + start_index,  
+                                     
+                 s21_strlen(src) - start_index);
+      ptr[s21_strlen(src) + s21_strlen(str)] = '\0';
+    }
+  }
+  return ptr;
 }
