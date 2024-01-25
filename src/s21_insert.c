e417db843ed9d41c8c96acaba5220ac1f986855d
Cainvene <cainvene@student.21-school.ru>
2024-01-25 13:14:06 +0600

    fis insert

--- src/s21_insert.c
+++ src/s21_insert.c
@@ -1,7 +1,9 @@
 #include "s21_string.h"
 
-void *s21_insert(const char *src, const char *str, size_t start_index) {  if (src == S21_NULL || str == S21_NULL || start_index > s21_strlen(src)) {
-    return S21_NULL;
+void *s21_insert(const char *src, const char *str, size_t start_index) { 
+char *res;
+ if (src == S21_NULL || str == S21_NULL || start_index > s21_strlen(src)) {
+    res = S21_NULL;
   }
   char *ptr = S21_NULL;
   if (src != S21_NULL && str != S21_NULL) {
@@ -11,13 +13,15 @@ void *s21_insert(const char *src, const char *str, size_t start_index) {  if (sr
       s21_memcpy(ptr, src, start_index);  
                                           
       s21_memcpy(ptr + start_index, str,
-                 s21_strlen(str));  /
+                 s21_strlen(str));  
       s21_memcpy(ptr + start_index + s21_strlen(str),
                  src + start_index,  
                                      
                  s21_strlen(src) - start_index);
       ptr[s21_strlen(src) + s21_strlen(str)] = '\0';
+      
     }
   }
-  return ptr;
+  res = ptr;
+  return res;
 }
